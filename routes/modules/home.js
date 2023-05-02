b4b3ab10d4d489c7ffa24a5ea3c8735ed596bd16
pleasesailas <pleasesailas@gmail.com>
2023-05-02 15:07:38 +0800

    feat: add login & register view page

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,18 +1,19 @@
 const express = require('express')
 const router = express.Router()
-const Records = require('../../models/record')
+const Record = require('../../models/record')
 
 
 router.get('/', (req, res) => {
-  res.render('index', { record })
-  Records.find()
+  // res.render('index')
+  Record.find()
     .lean()
     .then((record) => res.render('index', { record }))
     .catch(err => console.log(err))
-})
+}) 
 
 //create
 //read
+
 //update
 //delete
 
