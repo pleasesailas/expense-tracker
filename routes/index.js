356143ab7223af5a84ca99938ad9e7d8d38a5e52
pleasesailas <pleasesailas@gmail.com>
2023-05-03 09:37:19 +0800

    feat: add new view page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ const router = express.Router()
 
 const home = require('./modules/home')
 const users = require('./modules/users')
+const records = require('./modules/records')
 
+router.use('/records', records)
 router.use('/users', users)
 router.use('/', home)
 
